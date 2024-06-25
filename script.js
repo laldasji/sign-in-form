ef91778e1a2a13a5d67776392db917dc7edbede2
Abhinav Ashutosh <abhinavashutoshlaldas@gmail.com>
2024-06-25 14:06:16 +0530

    second pass

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let creditCard = document.querySelector('#creditCard');
 let phoneNo = document.querySelector('#phoneNo');
 let creditCardCVV = document.querySelector('#creditCardCVV');
+let firstName = document.querySelector('#firstName');
+let lastName = document.querySelector('#lastName');
 
 creditCard.addEventListener('input', (event) => {
     if (creditCard.validity.patternMismatch)
@@ -16,9 +18,23 @@ phoneNo.addEventListener('input', (event) => {
         phoneNo.setCustomValidity('');
 });
 
+firstName.addEventListener('input', event => {
+    if (firstName.validity.patternMismatch)
+        firstName.setCustomValidity('Bro, don\'t. Just don\'t');
+    else
+        firstName.setCustomValidity('');
+});
+
+lastName.addEventListener('input', event => {
+    if (firstName.validity.patternMismatch)
+        lastName.setCustomValidity('=_= are you have studip?');
+    else
+        lastName.setCustomValidity('');
+});
+
 creditCardCVV.addEventListener('input', event => {
     if (creditCardCVV.validity.patternMismatch)
         creditCardCVV.setCustomValidity('Hey come on?!');
     else
-        phoneNo.setCustomValidity('');
+        creditCardCVV.setCustomValidity('');
 });
